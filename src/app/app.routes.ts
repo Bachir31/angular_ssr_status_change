b939964ec77fd1d9d0b56a6adb297b8d6eef6367
Bachir31 <bouacheria.bachir@hotmail.fr>
2025-03-26 17:52:31 +0100

    delete empty route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
-        loadComponent: () => import('./app.component').then(m => m.AppComponent)
-    },
-    {
         path: 'entity/:entity',
         loadComponent: () => import('./components/entity/entity.component').then(m => m.EntityComponent)
     },
